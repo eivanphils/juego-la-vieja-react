f7999a71eef421e9b7168426c84825a47491fccd
Phils <phils.garcia@multicaja.cl>
2021-05-03 13:23:08 -0400

    archivos iniciales

---
+++ src/Square.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Square = () => {
+    
+    return (
+        <>
+            <button className="square">
+                {/* TODO */}
+            </button>
+        </>
+    )
+}
+
+export default Square;
